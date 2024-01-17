a47545ea21b8c3b2d2d84850daa4c39638341662
shwetha0505 <153724292+shwetha0505@users.noreply.github.com>
2024-01-17 10:59:26 +0000

    Add files via upload

---
+++ week5/day2/CreateLead.java
@@ -0,0 +1,24 @@
+package week5.day2;
+
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+
+
+public class CreateLead extends BaseClass{
+	@Test
+	public  void createLead() {
+		
+
+
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Create Lead")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("TestLeaf");
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Hari");
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("R");
+		driver.findElement(By.name("submitButton")).click();
+
+
+}
+}
